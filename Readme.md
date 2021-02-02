32f8461964c822e1af95177204a2cdef96d6ca39
tzirtzi <tzirtzi@gmail.com>
2021-02-02 15:38:21 +0200

    final working example

--- Readme.md
+++ Readme.md
@@ -4,13 +4,13 @@ gcloud auth login
 
 # Build this project: 
 
-docker build -f python3.8-alpine3.10.dockerfile -t marbbl:FastAPI_hello_test .
+docker build -f python3.8-alpine3.10.dockerfile -t marbbl:FastAPI_types_example .
 -- Notice that for production you may need to change log level to error 
 -- or at least warning in 'gunicorn_conf.py'. 
 
 # Run the image: 
 
-docker run -p 8000:80 marbbl:FastAPI_hello_test
+docker run -p 8000:80 marbbl:FastAPI_types_example
 -- feel free to run to whatever port externally, to change the internal port 80  
 -- change the 'gunicorn_conf.py' AND the EXPOSE command in .dockerfile 
 
@@ -20,6 +20,7 @@ Open your browser at http://127.0.0.1:8000
 
 There is a swagger-powered api documentation in http://127.0.0.1:8000/docs
 
+And also an Alternative API documentation with ReDoc http://127.0.0.1:8000/redoc
 
 # More info (from the creator of Fast API and maintener of base Docker images)
 
--- app/main.py
+++ app/main.py
@@ -1,42 +1,84 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
+
+from pydantic import BaseModel, Field, EmailStr
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_description="Wellcome to this fantastic API!")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World, welcome to this fantastic API...!"}
+
+
+@app.get("/api/fullname/v1/{firstName}/{lastName}", response_description="Capitalize input and return fullname")
+async def get_full_name(firstName:str, lastName:str):
+    fullName = f"{firstName.title()} {lastName.title()}"
+    return {"fullname": fullName}
+
+
+# using a class and input from request url #####################################################################################
+class UserName():
+
+    def __init__(self, firstName: str, lastName: str):
+        self.firstName = firstName if firstName else ""
+        self.lastName = lastName if lastName else ""
+
+    def getFullName(self) -> str: 
+        return f"{self.firstName.title()} {self.lastName.title()}"
+
+
+@app.get("/api/fullname/v2/{firstName}/{lastName}", response_description="Capitalize input, using Python Object and return fullname")
+async def get_full_name(firstName:str, lastName:str):
+    
+    print("Using the UserName Object...")
+    
+    obj = UserName(firstName, lastName)
+    fullName = obj.getFullName()
+    
+    return {"fullname": fullName}
+
+
+# Using pydantic and request body validation #####################################################################################
+
+class UserNameSchema(BaseModel):    # this is used in input validation, when put async function definition ie reqBody: UserNameSchema = Body(...)
+    firstName: str = Field()
+    lastName: str = Field(...,min_length=2)  # required field
+    email: EmailStr = Field()
+    year: int = Field(gt=18, lt=109)
+    class Config:                   # this will appear as an example in Swagger specs
+        schema_extra = {
+            "example": {
+                "firstName": "georgios em",
+                "lastName": "tzirtzilakis",
+                "email": "tzirtzi@gmail.com",
+                "year": 27
+            }
+        }
+
+
+@app.post("/api/fullname/v2", response_description="Capitalize input, using Python Object from request body and return fullname")
+async def get_full_name(reqBody: UserNameSchema = Body(...)):
+    
+    print("Using the request body...", reqBody)
+    
+    # You can use jsonable_encoder or alternatively, the following two lines
+    # reqDict = {k: v for k, v in reqBody.dict().items() if v is not None}
+    # userName = UserNameSchema(**reqDict)
+        
+    userName = jsonable_encoder(reqBody)    
+    print("After decoding...", userName)
+    fullName = f"{userName['firstName'].title()} {userName['lastName'].title()}"
+    
+    return {"fullname": fullName}
+
 
 # this is for dev purposes, use command line for prod, 
 # with workers and other params for optimal performance
 if __name__ == "__main__" : 
-    uvicorn.run ( app, host="0.0.0.0", port=8000)
+    uvicorn.run ( "main:app", host="0.0.0.0", port=8000, reload = True)
 
 
-## example without FastAPI use
-# import sys
 
-
-# class App:
-#     def __init__(self, scope):
-#         assert scope["type"] == "http"
-#         self.scope = scope
-
-#     async def __call__(self, receive, send):
-#         await send(
-#             {
-#                 "type": "http.response.start",
-#                 "status": 200,
-#                 "headers": [[b"content-type", b"text/plain"]],
-#             }
-#         )
-#         version = f"{sys.version_info.major}.{sys.version_info.minor}"
-#         message = f"Hello world! From Uvicorn with Gunicorn. Using Python {version}".encode(
-#             "utf-8"
-#         )
-#         await send({"type": "http.response.body", "body": message})
-
-
-# app = App
 
